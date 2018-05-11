13c4142cef39f4393aed096b3a804044ff1e2b20
gorengordon <goren@gorengordon.com>
2018-05-11 18:21:06 +0300

    pos neg and nut

--- nao_ros.py
+++ nao_ros.py
@@ -43,21 +43,22 @@ class NaoNode():
         rospy.init_node('nao_listener'+self.node_name)
         name='to_nao'+self.node_name
         rospy.Subscriber(name, String, self.parse_message)
-        rospy.spin()
+        # rospy.spin()    #FOR TEST!!!!!!!!!!
 
     def parse_message(self, message):
         # message is json string in the form of:  {'action': 'run_behavior', 'parameters': ["movements/introduction_all_0",...]}
         # eval the action and run with parameters.
         # For example, eval result could look like: self,say_text_to_speech(['hello','how are you?'])
-        message = str(message.data)
-        # print("parse_message", message)
+        # message = str(message.data)
+
+        message = str(message)  #FOR TEST!!!!!!!!!!
+
         message_dict = json.loads(message)
         action = str(message_dict['action'])
         if 'parameters' in message_dict:
             parameters = message_dict['parameters']
         else:
             parameters = ""
-        print("PARSE_MESSAGE")
         print(str("self." + action + "(" + str(parameters) + ")"))
         eval(str("self." + action + "(" + str(parameters) + ")"))
 
@@ -114,6 +115,7 @@ class NaoNode():
 
     def change_pose(self, data_str):
         # data_str = 'name1, name2;target1, target2;pMaxSpeedFraction'
+        print data_str
         info = data_str.split(';')
         pNames = info[0].split(',')
         pTargetAngles = [float(x) for x in info[1].split(',')]
@@ -130,4 +132,14 @@ class NaoNode():
         print("say_text_to_animated_speech", text)
         self.animatedSpeech.say(text, {"pitchShift": pitch})
 
-strat=NaoNode(sys.argv[1],sys.argv[2])
+    def get_angles(self):
+        names = "Body"
+        use_sensors = False
+        print("get_angles")
+        print "Command angeles:"
+        print(str(self.motionProxy.getAngles(names, use_sensors)))
+        use_sensors = True
+        print "Sensor angeles:"
+        return self.motionProxy.getAngles(names, use_sensors),self.motionProxy.getBodyNames(names)
+
+# strat=NaoNode(sys.argv[1],sys.argv[2])  #FOR TEST!!!!!!!!!!
--- test.py
+++ test.py
@@ -1,13 +1,74 @@
 import rospy
 from std_msgs.msg import String
-# from naoqi import ALProxy
-import sys
-# import almath
+from nao_ros import NaoNode
 import time
-import datetime
-import json
 
-rospy.init_node('matan')
+nao=NaoNode('192.168.0.100','0')
+#
+#
+# pNames= ['HeadYaw','HeadPitch']
+# robot_angles=[0.0,0.0]
 
-nao_movements = rospy.Publisher('to_nao', String, queue_size=10)
-nao_movements.publish('matan')
+def move_to_pose():
+
+    #center
+    nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadYaw,HeadPitch,LShoulderPitch,LShoulderRoll,LElbowYaw,LElbowRoll,LWristYaw,LHand,LHipYawPitch,LHipRoll,LHipPitch,LKneePitch,LAnklePitch,LAnkleRoll,RHipYawPitch,RHipRoll,RHipPitch,RKneePitch,RAnklePitch,RAnkleRoll,RShoulderPitch,RShoulderRoll,RElbowYaw,RElbowRoll,RWristYaw,RHand;-0.02,0.2,0.93,0.26,-0.45,-1.21,0.01,0.29,-0.6,0.2,-1.53,1.41,0.84,-0.0,-0.6,-0.2,-1.53,1.41,0.85,0.01,0.96,-0.3,0.53,1.24,-0.04,0.3;0.08''\\\"\"}')
+
+    #right
+    # nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadYaw,HeadPitch,LShoulderPitch,LShoulderRoll,LElbowYaw,LElbowRoll,LWristYaw,LHand,LHipYawPitch,LHipRoll,LHipPitch,LKneePitch,LAnklePitch,LAnkleRoll,RHipYawPitch,RHipRoll,RHipPitch,RKneePitch,RAnklePitch,RAnkleRoll,RShoulderPitch,RShoulderRoll,RElbowYaw,RElbowRoll,RWristYaw,RHand;-0.88,0.01,0.93,0.26,-0.45,-1.2,0.01,0.29,-0.6,0.2,-1.53,1.41,0.84,0.0,-0.6,0.0,-1.53,1.41,0.85,0.01,0.96,-0.29,0.53,1.26,-0.05,0.3;0.2''\\\"\"}')
+
+    #left
+    # nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadYaw,HeadPitch,LShoulderPitch,LShoulderRoll,LElbowYaw,LElbowRoll,LWristYaw,LHand,LHipYawPitch,LHipRoll,LHipPitch,LKneePitch,LAnklePitch,LAnkleRoll,RHipYawPitch,RHipRoll,RHipPitch,RKneePitch,RAnklePitch,RAnkleRoll,RShoulderPitch,RShoulderRoll,RElbowYaw,RElbowRoll,RWristYaw,RHand;0.88,0.01,0.92,0.27,-0.47,-1.22,0.01,0.29,-0.6,0.0,-1.53,1.41,0.84,0.0,-0.6,-0.2,-1.53,1.41,0.85,0.01,0.96,-0.3,0.53,1.24,-0.04,0.3;0.2''\\\"\"}')
+
+move_to_pose()
+
+def disagree():
+    counter = 0
+    basepose=nao.get_angles()[0][0]
+    print basepose
+    while counter <3:
+        nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadYaw;'+ str(basepose+0.2)+ ';0.08''\\\"\"}')
+        time.sleep(0.5)
+
+        nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadYaw;'+ str(basepose-0.2)+ ';0.08''\\\"\"}')
+        time.sleep(0.5)
+
+        counter+=1
+
+    nao.parse_message(        '{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadYaw;' + str(basepose) + ';0.08''\\\"\"}')
+
+def agree():
+    counter = 0
+    basepose = nao.get_angles()[0][1]
+    print basepose
+    while counter < 3:
+        nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadPitch;' + str(basepose + 0.2) + ';0.08''\\\"\"}')
+        time.sleep(0.5)
+
+        nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadPitch;' + str(basepose - 0.2) + ';0.08''\\\"\"}')
+        time.sleep(0.5)
+
+        counter += 1
+
+    nao.parse_message('{\"action\": \"change_pose\", \"parameters\": \"\\\"''HeadPitch;' + str(basepose) + ';0.08''\\\"\"}')
+
+
+def move_other_way(relative_to):
+
+
+
+
+
+def get_angles_nao():
+    a= nao.get_angles()
+
+    angels=''
+    names=''
+    for ang in a[0]:
+        b=round(ang,2)
+        angels=angels+','+str(b)
+
+    for nam in a[1]:
+        names=names+','+nam
+
+    return names[1:]+';'+angels[1:]
